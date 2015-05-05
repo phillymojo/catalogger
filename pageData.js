d63334617ecac2d79e8f4a5ef346b29b5c2e4a6e
Morgan Bonar <mbonar@gmail.com>
2015-05-04 21:03:57 -0700

    misssed pageData file

---
+++ pageData.js
@@ -0,0 +1,26 @@
+var pageData = {
+  headerData: {text: "Tori's Beanie Boo Catalog", color: "Red"},
+  currentInfo: {
+		latestPost: {
+			title: "Latest Post", 
+			content: "A single glance at the thing was sufficient to assure me that I was facing one of those long-extinct, prehistoric creatures whose fossilized remains are found within the outer crust as far back as the Triassic formation, a gigantic labyrinthodon.  And there I was, unarmed, and, with the exception of a loin cloth, as naked as I had come into the world.  I could imagine how my first ancestor felt that distant, prehistoric morn that he encountered for the first time the terrifying progenitor of the thing that had me cornered now beside the restless, mysterious sea."
+		},
+		nextitem: {
+			date: 'April 16',
+			item: {}
+		}
+  },
+  contentData: {text: "Content"},
+  itemData: {
+    items: [
+      {name: 'Tori'},
+      {name: 'Rocky'},
+      {name: 'Squaggle'},
+      {name: 'Chicken'},
+      {name: 'Butt'},
+      {name: 'Cooper'}
+    ]
+  }
+};
+
+module.exports = pageData;
